6e6c591ac905e33a180c85a6439c70a0b4dbb054
nastaranjalali <nastaranjalalibusiness@gmail.com>
2021-11-16 16:43:09 +0330

    feat:add logout button

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,7 +1,19 @@
-import React from "react";
-
+import { Button } from "@material-ui/core";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useContext } from "react";
 const Profile = () => {
-  return <div>this is profile</div>;
+  const { logout } = useContext(AuthContext);
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          logout();
+        }}
+      >
+        log out
+      </Button>
+    </div>
+  );
 };
 
 export default Profile;
